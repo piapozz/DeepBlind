0f6550e1e0f790aa2c3cb82d2929770136134cb9
Sein <mcd2376075@stu.o-hara.ac.jp>
2024-09-10 17:20:13 +0900

    Chinemachineを使ったカメラを実装。 カメラを考慮した移動の処理をInputSystemを使って実装。

--- Assets/Scripts/Intract.cs
+++ Assets/Scripts/Intract.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Intract : MonoBehaviour
 {
     [SerializeField] UIManager uiManager;
+
+    bool inputStay = false;                 // 入力を受け付けるかを管理する
+
+    public bool intract = false;
+
+    private void Update()
+    {
+        if (inputStay == true) Debug.Log("true");
+        else Debug.Log("false");
+    }
+
+    // ActionsのFireに登録されているキーが押されたときに入力値を取得
+    public void OnFire(InputValue inputValue)
+    {
+        if (inputStay == true) intract = true;
+        Debug.Log("インタ");
+    }
 
     void OnTriggerStay(Collider other)
     {
@@ -12,15 +30,23 @@ public class Intract : MonoBehaviour
 
         if (door != null)
         {
+            // インタラクトの入力を受け付けるようにする
+            inputStay = true;
+ 
             if (door.GetOpen() == true)
                 uiManager.DisplayIntractUI("Close:E");
             else
                 uiManager.DisplayIntractUI("Open:E");
-
-            if (Input.GetKeyDown(KeyCode.E) == true)
+            
+            // if (Input.GetKeyDown(KeyCode.E) == true)
+            if(intract == true)
             {
+                Debug.Log("開閉");
                 door.OpenDoor();
             }
+
+            // インタラクトを無効にする
+            intract = false;
         }
 
         Item item = other.GetComponent<Item>();
@@ -33,6 +59,9 @@ public class Intract : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
+        // インタラクトの入力受付を無効化する
+        inputStay = false;
+
         uiManager.DisableIntractUI();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.EventSystems.StandaloneInputModule;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] GenerateStage generateStage;
     [SerializeField] CharacterController characterController;
+    [SerializeField] GameObject camera;
+
+    [SerializeField] private InputActionReference hold;             // 長押しを受け取る対象のAction
+
+    // プレイヤーのステータス値
+    public struct PlayerStatus
+    {   
+        public float stamina;                                       // スタミナ量
+        public float speed;                                         // 動く速さ
+        public float fear;                                          // 怖気度
+        public float soundRange;                                    // プレイヤーが出してしまう音の範囲
+    }
 
     Vector3 offsetgenPos = new Vector3(0, 1.5f, 0);
-    float moveSpeed = 5.0f;
-    Vector3 moveVec;
+    float moveSpeed = 10.0f;
+    float rotateSpeed = 1.0f;
     Vector2 rotateVec;
+
+    Vector2 inputMove;                                              // InputSystemで得たWASDの入力値を管理する
+    Vector2 inputCursor;                                            // InputSystemで得たマウスのカーソルの入力値を管理する
+    float inputDash;                                                // InputSystemを使ってダッシュの管理
+
+    // 移動量
+    Vector3 moveVec;
+
+    private void Awake()
+    {
+        if (hold == null) return;
+
+        // InputActionReferenceのholdにハンドラを登録する
+        // hold.action.started += OnDash;
+        hold.action.performed += OnDash;
+
+        // 入力を受け取るために有効化
+        hold.action.Enable();
+    }
 
     void Start()
     {
@@ -20,34 +54,69 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+
+        // カメラを考慮した移動(InputSystemとCharacterController使用)
         Move();
 
-        Rotate();
+        // カメラの回転
+        // Rotate();
+
     }
 
+    // DeepBlindのアクションマップのMoveに登録されているキーが押されたときに入力値を取得
+    public void OnMove(InputAction.CallbackContext context)
+    {   
+        inputMove = context.ReadValue<Vector2>();
+    }
+
+    // ActionsのLookに割り当てられている入力があったなら実行（未実装）
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        inputCursor = context.ReadValue<Vector2>();
+    }
+
+    // ActionsのDashに割り当てられている入力があったなら実行
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        inputDash = context.ReadValue<float>();
+    }
+
+    // 移動
     void Move()
     {
-        Vector3 verMove = new Vector3(transform.forward.x, 0, transform.forward.z) * Input.GetAxis("Vertical");
-        Vector3 horiMove = transform.right * Input.GetAxis("Horizontal");
+        // ダッシュのキーが押されたとき移動する速さを変更する
+        if (inputDash != 0) moveSpeed = 5.0f;
+        else moveSpeed = 2.0f;
 
-        moveVec = horiMove + verMove;
+        // 入力でもらった値を使って移動量を計算
+        moveVec = new Vector3(inputMove.x, 0, inputMove.y);
+
+        // 値の正規化
         moveVec.Normalize();
-        moveVec = moveVec * moveSpeed;
 
-        // Input.systemに直す
+        // カメラの角度分だけ移動量を回転
+        moveVec = Quaternion.Euler(0, camera.gameObject.transform.eulerAngles.y, 0) * moveVec;
 
-        characterController.Move(moveVec * Time.deltaTime);
+        // フレームごとの移動量を計算し動かす
+        characterController.Move(moveVec * Time.deltaTime * moveSpeed);
     }
 
+    // 回転
     void Rotate()
     {
-        rotateVec.x = Input.GetAxis("Mouse X");
-        rotateVec.y = Input.GetAxis("Mouse Y");
+        Quaternion temp = transform.rotation;
 
-        transform.rotation = 
-            Quaternion.Euler(0, rotateVec.x, 0) * 
-            transform.rotation * 
-            Quaternion.Euler(-rotateVec.y, 0, 0);
+        temp = Quaternion.Euler(0, inputCursor.x * rotateSpeed, 0) * temp * Quaternion.Euler(-inputCursor.y * rotateSpeed, 0, 0);
+
+        Vector3 angle = temp.eulerAngles;
+
+        float angleK = Mathf.Repeat(angle.x + 180, 360) - 180;
+
+        Debug.Log(inputCursor.x);
+        Debug.Log(angleK);
+
+        if (!(angleK > -75.0f && angleK < 75.0f)) temp = transform.rotation;
+
     }
 
     // どの区画にいるかを返す関数
