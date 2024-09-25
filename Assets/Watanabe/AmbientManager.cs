79a944696886767c5ba1dac739fc71912a12f79f
Sein <mcd2376075@stu.o-hara.ac.jp>
2024-09-25 12:28:58 +0900

    Playerスクリプトのデバッグログを削除 自身のファイルのいらないもの削除

---
+++ Assets/Watanabe/AmbientManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientManager : MonoBehaviour
+{
+    [SerializeField] Player player;
+
+    // プレイヤーから物音を受け取る
+
+
+    // 敵から物音を受け取る
+
+    // プレイヤーと敵が出している物音を比較
+
+    // 敵が聞こえたか聞こえてないかを返す
+
+
+}
